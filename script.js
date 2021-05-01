4e3e69393ece93467d7c62935b83ee17543d286d
nguyenbryan54 <bln4@pdx.edu>
2021-05-01 00:17:59 -0700

    update

--- script.js
+++ script.js
@@ -35,3 +35,85 @@ $kobe3.waypoint(function(direction) {
 }, { offset: '20%'});
 
 
+
+var $kobe4 = $('.kobe4');
+
+$kobe4.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe4.attr('src', 'https://media.giphy.com/media/FTsKB8nn88Bz73JjK8/giphy.gif');
+  } else {
+    $kobe4.attr('src', 'https://d1l5jyrrh5eluf.cloudfront.net/wp-content/uploads/2015/12/kobe-bryant.jpg')
+  }
+}, { offset: '20%'});
+
+
+
+var $kobe5 = $('.kobe5');
+
+$kobe5.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe5.attr('src', 'https://media.giphy.com/media/26ufm9YcCl3Cb7dwk/giphy.gif');
+  } else {
+    $kobe5.attr('src', 'https://sneakerbardetroit.com/wp-content/uploads/2016/04/kobe-bryant-last-game.jpg')
+  }
+}, { offset: '0%'});
+
+
+var $kobe6 = $('.kobe6');
+
+$kobe6.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe6.attr('src', 'https://media.giphy.com/media/l0HlUH6eDIyq3ASwo/giphy.gif');
+  } else {
+    $kobe6.attr('src', 'https://lakeshowlife.com/wp-content/uploads/getty-images/2017/11/81583435-nba-finals-game-5-boston-celtics-v-los-angeles-lakers.jpg.jpg')
+  }
+}, { offset: '50%'});
+
+
+var $kobe7 = $('.kobe7');
+
+$kobe7.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe7.attr('src', 'https://media.giphy.com/media/l0HlUH6eDIyq3ASwo/giphy.gif');
+  } else {
+    $kobe7.attr('src', 'https://lakeshowlife.com/wp-content/uploads/getty-images/2017/11/81583435-nba-finals-game-5-boston-celtics-v-los-angeles-lakers.jpg.jpg')
+  }
+}, { offset: '50%'});
+
+
+
+
+var $kobe8 = $('.kobe8');
+
+$kobe8.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe8.attr('src', 'https://i.makeagif.com/media/12-07-2015/OvTNfs.gif');
+  } else {
+    $kobe8.attr('src', 'https://static01.nyt.com/images/2013/04/14/sports/subLAKERS/subLAKERS-superJumbo.jpg')
+  }
+}, { offset: '50%'});
+
+
+
+
+
+var $kobe9 = $('.kobe9');
+
+$kobe9.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe9.attr('src', 'https://thumbs.gfycat.com/InfamousTerrificBeaver-max-1mb.gif');
+  } else {
+    $kobe9.attr('src', 'https://pbs.twimg.com/media/EPTZOoPU8A896-A.jpg')
+  }
+}, { offset: '20%'});
+
+
+var $kobe10 = $('.kobe10');
+
+$kobe10.waypoint(function(direction) {
+  if (direction == 'down'){
+    $kobe10.attr('src', 'https://j.gifs.com/YEYWvY.gif');
+  } else {
+    $kobe10.attr('src', 'https://thestateindia.com/wp-content/uploads/2020/01/basketball-veteran-Coby-Bryan-and-his-daughter-die-in-helicopter-crash.png')
+  }
+}, { offset: '20%'});
